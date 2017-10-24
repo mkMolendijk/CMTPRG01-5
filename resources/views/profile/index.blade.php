27b42f06a862a7a0494030d8eead805ff3944ab8
Mark Molendijk <mark.molendijk@dpdk.com>
2017-10-24 23:30:39 +0200

    Added user specific games list to profile page

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -43,5 +43,51 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        Games
+                        <div class="game-controls pull-right">
+                            <div class="btn-group">
+                                <a href="#" id="list" class="btn btn-default btn-sm">
+                                    <span class="glyphicon glyphicon-th-list"></span>
+                                    List
+                                </a>
+                                <a href="#" id="grid" class="btn btn-default btn-sm">
+                                    <span class="glyphicon glyphicon-th"></span>
+                                    Grid
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <!-- Table -->
+                    @include('profile/games-list')
+
+                    <!-- Grid -->
+                        @include('profile/games-grid')
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+@endsection
+
+@section('footer')
+    <script>
+        $(document).ready(function () {
+            $('#list').click(function (event) {
+                event.preventDefault();
+                $('#games-list').css('display', 'block');
+                $('#games-flex-grid').css('display', 'none');
+            });
+            $('#grid').click(function (event) {
+                event.preventDefault();
+                $('#games-flex-grid').css('display', 'flex');
+                $('#games-list').css('display', 'none');
+            });
+        });
+    </script>
 @endsection
