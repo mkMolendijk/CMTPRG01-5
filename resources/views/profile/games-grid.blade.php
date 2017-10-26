aa0c94c23e1814823b1a5bfa895e0e27e6b01982
Mark Molendijk <mark.molendijk@dpdk.com>
2017-10-27 00:01:25 +0200

    Refactor query to eager loading on profile page

--- resources/views/profile/games-grid.blade.php
+++ resources/views/profile/games-grid.blade.php
@@ -1,11 +1,16 @@
 <div id="games-flex-grid">
     @foreach($games as $game)
         <div class="grid-item">
-            {{--<a class="grid-link" href="{{ url('/dashboard/game-detail/'.$game->id) }}">--}}
-                <div class="thumbnail">
+            @if ($game->user->admin === 1)
+                <a class="list-link" href="{{ url('/admin/game-detail/'.$game->id) }}">
+            @else
+                <a class="list-link" href="{{ url('/dashboard/game-detail/'.$game->id) }}">
+            @endif
+                    <div class="thumbnail">
                     <img class="thumbnail grid-img" src="{{ $game->image }}">
                     <div class="caption">
                         <h3 class="game-title">{{ $game->title }}</h3>
+                        <p>{{ $game->genre->title }}</p>
                         <p>{{ $game->description }}</p>
                     </div>
                 </div>
--- resources/views/profile/games-list.blade.php
+++ resources/views/profile/games-list.blade.php
@@ -1,16 +1,20 @@
 <div id="games-list">
     @foreach($games as $game)
         <div class="list-item spacing-bottom">
-            <a class="list-link" href="{{ url('/dashboard/game-detail/'.$game->id) }}">
-                <div class="thumbnail">
-                    <img class="thumbnail list-img pull-left" src="{{ $game->image }}">
-                    <div class="caption">
-                        <h3 class="game-title">{{ $game->title }}</h3>
-                        {{--<p>{{ $genreTitle }}</p>--}}
-                        <p>{{ $game->description }}</p>
-                    </div>
-                </div>
-            </a>
+            @if ($game->user->admin === 1)
+                <a class="list-link" href="{{ url('/admin/game-detail/'.$game->id) }}">
+                    @else
+                        <a class="list-link" href="{{ url('/dashboard/game-detail/'.$game->id) }}">
+                            @endif
+                            <div class="thumbnail">
+                                <img class="thumbnail list-img pull-left" src="{{ $game->image }}">
+                                <div class="caption">
+                                    <h3 class="game-title">{{ $game->title }}</h3>
+                                    <p>{{ $game->genre->title }}</p>
+                                    <p>{{ $game->description }}</p>
+                                </div>
+                            </div>
+                        </a>
         </div>
     @endforeach
 </div>
