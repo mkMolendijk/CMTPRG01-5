d86853dcad161df84e840d792c965b937f426c34
Mark Molendijk <mark.molendijk@dpdk.com>
2017-11-15 15:12:17 +0100

    Changed navbar on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,27 +14,31 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="top-left links">
-        <p>
-            Mark Molendijk
-        </p>
-    </div>
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                @if (Auth::user()['admin'])
-                    <a href="{{ url('/admin') }}">Admin panel</a>
-                @else
-                    <a href="{{ url('/dashboard') }}">Dashboard</a>
-                @endif
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-                    <a href="{{ route('register') }}">Register</a>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light spacing-bottom">
+    <a class="navbar-brand" href="#">Mark Molendijk</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        @if (Route::has('login'))
+            <div class="navbar-nav">
+                @auth
+                    @if (Auth::user()['admin'])
+                        <a class="nav-item nav-link" href="{{ url('/admin') }}">Admin panel</a>
+                    @else
+                        <a class="nav-item nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
+                    @endif
+                    @else
+                        <a class="nav-item nav-link" href="{{ route('login') }}">Login</a>
+                        <a class="nav-item nav-link" href="{{ route('register') }}">Register</a>
                     @endauth
-        </div>
-    @endif
+            </div>
+        @endif
+    </div>
 </nav>
+
 <div class="jumbotron">
     <div class="container-fluid">
         <div class="row">
